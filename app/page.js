4fb42bc94cfdd8f1de851fe670bc717b69ea3225
Nicolás Fojo <nicolasfojo88@gmail.com>
2026-06-16 18:21:35 -0300

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,25 @@
+export default function Home() {
+  return (
+    <main style={{
+      width:'100vw',
+      height:'100vh',
+      background:'#000',
+      display:'flex',
+      alignItems:'center',
+      justifyContent:'center'
+    }}>
+      <video
+        src="/video.mp4"
+        autoPlay
+        loop
+        controls={false}
+        playsInline
+        style={{
+          width:'100%',
+          height:'100%',
+          objectFit:'contain'
+        }}
+      />
+    </main>
+  );
+}
